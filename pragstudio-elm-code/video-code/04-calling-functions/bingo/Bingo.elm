08fe2b4cecaf6091f0fe9b94de793afc24f1b8d4
Jean-François Beauchef <jean-francois@beauchef.com>
2016-11-15 21:45:22 -0500

    Added project code

---
+++ pragstudio-elm-code/video-code/04-calling-functions/bingo/Bingo.elm
@@ -0,0 +1,13 @@
+module Bingo exposing (..)
+
+import Html
+
+
+--main =
+--    Html.text (String.repeat 3 (String.toUpper "Mike's Game #3"))
+
+
+main =
+    "Mike's Game #3"
+        |> String.toUpper
+        |> Html.text
