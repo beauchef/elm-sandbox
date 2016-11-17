970ef6516dd0cb061f2c6406580b6d935049f06e
Jean-François Beauchef <jean-francois@beauchef.com>
2016-11-16 22:15:50 -0500

    Added some HTML to display

--- pragstudio-elm-code/starter-project/bingo/Bingo.elm
+++ pragstudio-elm-code/starter-project/bingo/Bingo.elm
@@ -1,7 +1,48 @@
+{-
+   Bingo test module
+   Example from: http://www.pragmaticstudio.com/
+-}
+
+
 module Bingo exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+playerInfo name gameNumber =
+    name ++ " - Game #" ++ (toString gameNumber)
+
+
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2 [ id "info", class "alert" ] [ playerInfoText ]
+
+
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter =
+    footer []
+        [ a [ href "http://elm-lang.org/" ]
+            [ text "Power by Elm" ]
+        ]
+
+
+view =
+    div [ class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer "Nicole" 4
+        , viewFooter
+        ]
 
 
 main =
-    Html.text "Hello, JF!"
+    view
